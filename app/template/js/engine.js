1b3919d31007eba9110d68cafe66033d30680472
Aleksey <kronos2003@gmail.com>
2019-06-23 13:25:14 +0300

    Добавлена всплывающая форма callback

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -99,5 +99,62 @@ window.onscroll = function(){
 	// 		document.querySelector('.mobilepagedown').classList.remove('hide')
 	// 	} catch(e){}
 	// }
-
 };
+
+
+
+
+window.onload = function(){
+	[].forEach.call(document.querySelectorAll('[data-toggle="mymodal"]'),function(el,i){
+		el.addEventListener('click', function(e){
+			e.preventDefault();
+			let target = el.dataset.target;
+				modal = new Popup(target);
+				modal.open();
+				this.classList.add('active')
+			})
+	})		
+}
+
+// popup on homepage
+class Popup{
+	constructor(name){
+		this.name = name;
+	}
+	open(){
+		// create overlay
+		const newOverlay = document.createElement('div');
+		newOverlay.className ='transparent-overlay';
+		document.querySelector('body').appendChild(newOverlay);
+		// show overlay
+		document.querySelector('.transparent-overlay').addEventListener('click', function(){
+			modal.close();
+		});
+
+		// GENERAL EVENT - ONKEYDOWN
+		document.onkeydown = function(evt) {
+		    evt = evt || window.event;
+		    let isEscape = false;
+		    if ("key" in evt) {
+		        isEscape = (evt.key == "Escape" || evt.key == "Esc");
+		    } else {
+		        isEscape = (evt.keyCode == 27);
+		    }
+		    if (isEscape) {
+		    	modal.close();
+		    }
+		};
+		// console.log(this.name);
+		
+		// show popup
+		document.querySelector(this.name).classList.add('show');
+	}
+	close(){
+		document.querySelector('.transparent-overlay').remove();
+		// hide popup
+		document.querySelector(this.name).classList.remove('show');
+
+		// remove active class
+		// document.querySelector('.cats .active').classList.remove('active')
+	}
+}
